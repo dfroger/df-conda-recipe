086cee513b0b6352ee7c8eb5c40c03d9b738366f
David Froger <david.froger@inria.fr>
2014-07-13 00:20:19 +0200

    blas example level 2

--- atlas/example/example_blas.c
+++ atlas/example/example_blas.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
+#include "cblas.h"
 
-#define N 5
+#define N 4
 
-int main(int argc, char **argv)
+int main()
 {
+  // Level 1
   float alpha = 0.5;
 
-  float x[N]  = {0,2,4,6,8};
+  float x[N]  = {0,2,4,6};
   int incx = 1;
 
-  float y[N] = {0,100,200,300,400};
+  float y[N] = {0,10,20,30};
   float incy = 1;
 
   cblas_saxpy(N, alpha, x, incx, y, incy);
 
-  printf("%f %f %f %f %f\n",y[0],y[1],y[2],y[3],y[4]);
+  printf("%f %f %f %f\n",y[0],y[1],y[2],y[3]);
+
+  // Level 2
+  enum CBLAS_ORDER order = CblasRowMajor;
+  enum CBLAS_UPLO uplo = CblasUpper;
+  enum CBLAS_TRANSPOSE trans = CblasNoTrans;
+  enum CBLAS_DIAG diag = CblasNonUnit;
+  float A[N*N] = { 1,  2,  3,  4,
+                   0,  5,  6,  7,
+                   0,  0,  8,  9,
+                   0,  0,  0, 10};
+  int lda = N;
+  float b[N] = {20,38,43,30};
+  int incb = 1;
+  cblas_strsv(order,uplo,trans,diag,N,A,lda,b,incb);
+  printf("%f %f %f %f\n",b[0],b[1],b[2],b[3]);
 
   return 0;
 }
