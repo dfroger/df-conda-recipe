e8026ece9c309ab94aefbf7c2dac88ae13db7e48
froger <froger@macchabee.rocq.inria.fr>
2014-12-01 20:33:55 +0100

    Fix GNU vs BSD sed compatibility

--- metis/build.sh
+++ metis/build.sh
@@ -8,9 +8,8 @@ then
 elif [ "$long_bit" == "64" ]
 then
     echo "Configure metis for 64 bit architecture"
-    sed 's/#define IDXTYPEWIDTH 32/#define IDXTYPEWIDTH 64/' -i include/metis.h
-    sed 's/#define REALTYPEWIDTH 32/#define REALTYPEWIDTH 64/' -i include/metis.h
-    echo 64
+    sed -i.bak 's/#define IDXTYPEWIDTH 32/#define IDXTYPEWIDTH 64/' include/metis.h
+    sed -i.bak 's/#define REALTYPEWIDTH 32/#define REALTYPEWIDTH 64/' include/metis.h
 else
     echo "Expected 'getconfig LONG_BIT' to be 32 or 64, but got $long_bit"
     exit 1
