8c756f6e6001d635d07e2b8cd44f192818f50fce
David Froger <david.froger@inria.fr>
2014-12-07 14:03:14 +0100

    mpich recipe for OS X

--- mpich/build.sh
+++ mpich/build.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export CC=clang
+export CXX=clang
+
+export LIBS="-lgfortran"
+export DYLD_LIBRARY_PATH=$PREFIX/lib
+
+export CFLAGS='-arch x86_64'
+export CXXFLAGS='-arch x86_64'
+
+export FCFLAGS="-arch x86_64"
+export FFLAGS="-arch x86_64"
+
+export LDFLAGS="-arch x86_64 -L$PREFIX/lib -L/Developer/SDKs/MacOSX10.5.sdk/usr/lib"
+
 ./configure --prefix=$PREFIX
 make
 make install
