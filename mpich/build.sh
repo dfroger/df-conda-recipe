faf18b7a601210b4c73e5dbb109f2a87d5f369c9
David Froger <david.froger@inria.fr>
2014-12-15 12:07:19 +0100

    Fix dynamic linking

--- mpich/build.sh
+++ mpich/build.sh
@@ -16,8 +16,11 @@ export CXXFLAGS='-arch x86_64'
 export FCFLAGS="-arch x86_64"
 export FFLAGS="-arch x86_64"
 
-export LDFLAGS="-arch x86_64 -L$PREFIX/lib -L/Developer/SDKs/MacOSX10.5.sdk/usr/lib"
+GFORTRAN_DYNAMIC_LIB_DIR=$PREFIX/lib
+SDK_10_5_STATIC_LIB_DIR=/Developer/SDKs/MacOSX10.5.sdk/usr/lib
+
+export LDFLAGS="-arch x86_64 -L$GFORTRAN_DYNAMIC_LIB_DIR -L$SDK_10_5_STATIC_LIB_DIR"
 
 ./configure --prefix=$PREFIX
-make
+make -j$CPU_COUNT
 make install
