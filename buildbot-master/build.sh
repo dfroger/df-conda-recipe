7f09782a8778cd2c6e5cec3b0ea8af62b1bf6944
David Froger <david.froger@inria.fr>
2015-01-12 21:52:55 +0100

    Fix buildbot-master recipe

--- buildbot-master/build.sh
+++ buildbot-master/build.sh
@@ -1,2 +1,8 @@
 cd master
-$PYTHON setup.py install
+$(which pip) install .
+
+# note:
+# If not using pip but:
+#    python setup.py install
+# Create the package, but it can not be used:
+#     pkg_resources.ResolutionError: No script named 'buildbot'
