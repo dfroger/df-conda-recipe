1cb4181db62cc2e7068fe74e981bcf1e01fbfd21
David Froger <david.froger@inria.fr>
2015-02-04 13:38:32 +0100

    Forgotten to commit this

--- inrimage/build.sh
+++ inrimage/build.sh
@@ -3,22 +3,33 @@
 # Note:
 #  requires gfortran
 
-if [ -z "$OSX_ARCH" ]
-then
-  FLAGS="LDFLAGS=-L$PREFIX/lib F77=/usr/bin/gfortran"
-else
-  FLAGS="CFLAGS=-Wno-return-type F77=/usr/local/bin/gfortran"
-fi
 
 tar xvzf $RECIPE_DIR/inrimage-4.6.5-fonts.tar.gz -C ..
 
-./configure \
-    --disable-build-fonts \
-    --prefix=$PREFIX \
-    $FLAGS
+if [ -z "$OSX_ARCH" ]
+then
+    ./configure \
+        --disable-build-fonts \
+        --prefix=$PREFIX \
+        F77=/usr/bin/gfortran \
+        LDFLAGS=-L$PREFIX/lib
+else
+    ./configure \
+        --disable-build-fonts \
+        --prefix=$PREFIX \
+        F77=/usr/local/bin/gfortran \
+        CFLAGS='-Wno-return-type -std=c89'
+fi
 
 make # Note: -j make the build failed.
 make install
 
 # Remove this broken symbolic link.
 rm $PREFIX/share/inrimage/WWW
+
+# sur mac os x, compile error:
+
+# iouvct.c:205:1: error: conflicting types for 'c_fermct'
+# ../h/inrimage/contour.h:189:17: note: previous declaration is here
+# void            c_fermct(struct fc_cont *nfc);
+
