3c3c25f353b1525b3269224bc43bb5bbc9b048de
David Froger <david.froger@inria.fr>
2015-04-29 14:36:51 +0200

    mpich_cxx recipe

--- mpich_cxx/build.sh
+++ mpich_cxx/build.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-
 set -o errexit
 set -o pipefail
-set -o nounset
 
-export CC=clang
-export CXX=clang++
+if [ -z "$OSX_ARCH" ]
+then
+    :
+else
+    export CC=clang
+    export CXX=clang++
 
-export CFLAGS='-arch x86_64'
-export CXXFLAGS='-arch x86_64'
+    export CFLAGS='-arch x86_64'
+    export CXXFLAGS='-arch x86_64'
 
-export FCFLAGS="-arch x86_64"
-export FFLAGS="-arch x86_64"
+    SDK_10_5_STATIC_LIB_DIR=/Developer/SDKs/MacOSX10.5.sdk/usr/lib
+    export LDFLAGS="-arch x86_64 -L$SDK_10_5_STATIC_LIB_DIR"
+fi
 
-SDK_10_5_STATIC_LIB_DIR=/Developer/SDKs/MacOSX10.5.sdk/usr/lib
+./configure \
+    --prefix=$PREFIX \
+    --disable-fortran \
+    --disable-fc \
+    --disable-f77 \
+    --enable-fast=all,O3 \
+    --enable-shared
 
-export LDFLAGS="-arch x86_64 -L$SDK_10_5_STATIC_LIB_DIR"
-
-./configure --prefix=$PREFIX --disable-fc --disable-f77
 make -j$CPU_COUNT
+
 make install
