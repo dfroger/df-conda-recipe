7a6831e87c8045aa8061d98f835d10d4789d17b2
David Froger <david.froger@mailoo.org>
2016-08-05 11:34:03 +0200

    Add monetdb recipe

---
+++ monetdb/build.sh
@@ -0,0 +1,19 @@
+CONDA_CFLAGS="-I$CONDA_PREFIX/include"
+CONDA_LDFLAGS="-L$CONDA_PREFIX/lib -Wl,-rpath,$CONDA_PREFIX/lib"
+CONDA_LIB="-lncurses -lxml2"
+# ncurses required by readline
+
+./configure \
+    pcre_CFLAGS="$CONDA_CFLAGS" \
+    pcre_LIBS="$CONDA_LDFLAGS -lpcre" \
+    libxml2_CFLAGS="$CONDA_CFLAGS -I$CONDA_PREFIX/include/libxml2" \
+    libxml2_LIBS="$CONDA_LDFLAGS" \
+    CFLAGS="$CONDA_CFLAGS" \
+    LDFLAGS="$CONDA_LDFLAGS" \
+    LIBS="$CONDA_LIB" \
+    --with-readline=$CONDA_PREFIX \
+    --prefix=$PREFIX
+
+make
+
+make install
